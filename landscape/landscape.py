1f64f1eefac0e174c0c051eeb48fe98cf14112ab
Jaz Walker <playallday723@gmail.com>
2019-09-23 15:38:10 -0500

    This is a simple landscape.

--- landscape/landscape.py
+++ landscape/landscape.py
@@ -26,23 +26,7 @@ hank.left(90)
 hank.forward(400)
 hank.end_fill()
 
-bill.penup()
-bill.forward(600)
-bill.pendown()
-bill.fillcolor("dodger blue")
-bill.begin_fill()
-bill.left(90)
-bill.forward(400)
-bill.left(90)
-bill.forward(1200)
-bill.left(90)
-bill.forward(400)
-bill.left(90)
-bill.forward(1200)
-bill.end_fill()
 
-dale.penup()
-dale.goto(-200, 0)
 
 
 
