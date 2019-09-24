6a8086feecbe3b053c277dfcaff2dd35a1e897b0
Jaz Walker <playallday723@gmail.com>
2019-09-24 15:38:16 -0500

    This is a simple landscape.

---
+++ landscape 2/landscape.py
@@ -0,0 +1,34 @@
+import turtle
+
+turtle.shape("turtle")
+
+hank = turtle.Turtle()
+scn = turtle.Screen()
+bill = turtle.Turtle()
+dale = turtle.Turtle()
+boomhauer = turtle.Turtle()
+
+hank.shape("turtle")
+bill.shape("square")
+dale.shape("circle")
+
+hank.pencolor("black")
+hank.pensize(3)
+hank.fillcolor("lime green")
+hank.begin_fill()
+hank.forward(600)
+hank.backward(1200)
+hank.right(90)
+hank.forward(400)
+hank.left(90)
+hank.forward(1200)
+hank.left(90)
+hank.forward(400)
+
+hank.end_fill()
+
+
+
+
+
+turtle.exitonclick()
